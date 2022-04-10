c1ffab18cf24b0ddb22a6b9d94ce5111cae2f3b3
wojtek123 <1889wojtek@gmail.com>
2022-04-10 21:38:34 +0200

    Lab4a. Dodanie klasy TestFileRead

--- src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
@@ -3,6 +3,8 @@ package pl.lublin.wsei.java.cwiczenia;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+
+
 public class HelloController {
     @FXML
     private Label welcomeText;
--- src/main/java/pl/lublin/wsei/java/cwiczenia/test/TestFileRead.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/test/TestFileRead.java
@@ -1,6 +1,26 @@
+
+
 package pl.lublin.wsei.java.cwiczenia.test;
 
+import javax.crypto.spec.PSource;
+import java.io.*;
+import java.nio.channels.Pipe;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
 public class TestFileRead {
+    public static void main(String[] args) {
+        try{
+            String contents = new String(Files.readAllBytes(Paths.get("src/gusInfoGraphic.xml")));
+            System.out.printf("ZAWARTOSC PLIKU : \n");
+            System.out.print(contents);
+        }
+        catch(IOException e){
+            System.out.println("Błąd wczytywania pliku :"+ e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+    }
 
 
 }
