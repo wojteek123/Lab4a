0907c2fda2e25eec1869fba3c931c10184b9d2df
wojtek123 <1889wojtek@gmail.com>
2022-04-11 16:16:00 +0200

    Lab4a. dodanie klasy GusInfoGraphicList

---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/Infografika.java
@@ -0,0 +1,12 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+public class Infografika {
+    String tytul;
+    String adres_strony;
+    String szerokosc;
+    public Infografika(String tekst){
+        String s = tekst.toUpperCase();
+
+    }
+
+}
