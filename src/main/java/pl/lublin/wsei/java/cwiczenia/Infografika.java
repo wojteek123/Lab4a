a36ddab643d4af5f98990c9ed388f7a20d07704c
wojtek123 <1889wojtek@gmail.com>
2022-04-11 23:34:49 +0200

    Lab4a. dodanie klasy GusInfoGraphicList

--- src/main/java/pl/lublin/wsei/java/cwiczenia/Infografika.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/Infografika.java
@@ -1,12 +1,61 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Infografika {
     String tytul;
     String adres_strony;
+    String adres_grafiki;
+    String adres_miniaturki;
     String szerokosc;
-    public Infografika(String tekst){
-        String s = tekst.toUpperCase();
+    String wysokosc;
 
-    }
+    public Infografika(String tekst){
+        Pattern pat = Pattern.compile("<title><!>\\[CDATA\\[(.*)\\]\\]");
+        Matcher m = pat.matcher(tekst);
+            if(m.find())
+                tytul=m.group(1);
+            else
+                tytul="";
+
+        pat = Pattern.compile("<link>(.*)</link>");
+        m = pat.matcher(tekst);
+            if(m.find())
+                adres_strony=m.group(1);
+            else
+                adres_strony="";
+
+
+
+            pat = Pattern.compile("width=\"(.*)\"height");
+            m = pat.matcher(tekst);
+            if(m.find())
+                szerokosc=m.group(1);
+            else
+                szerokosc= "0";
+
+           pat = Pattern.compile("width=\"(.*)\"height");
+           m = pat.matcher(tekst);
+           if(m.find())
+                wysokosc=m.group(1);
+           else
+                wysokosc= "0";
+
+            pat = Pattern.compile("width=\"(.*)\"height");
+            m = pat.matcher(tekst);
+            if(m.find())
+                adres_grafiki=m.group(1);
+            else
+                adres_grafiki= "0";
+
+            pat = Pattern.compile("width=\"(.*)\"height");
+            m = pat.matcher(tekst);
+            if(m.find())
+                adres_miniaturki=m.group(1);
+            else
+                adres_miniaturki= "0";
+
+                }
 
 }
---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/TestInfografika.java
@@ -0,0 +1,14 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+import pl.lublin.wsei.java.cwiczenia.Infografika;
+
+public class TestInfografika {
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+}
