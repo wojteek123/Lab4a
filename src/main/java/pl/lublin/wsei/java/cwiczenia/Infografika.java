01a6a9b14239f880552a8da80b633bc9e9395d37
wojtek123 <1889wojtek@gmail.com>
2022-04-12 10:31:14 +0200

    Lab4a. TestInfografika

--- src/main/java/pl/lublin/wsei/java/cwiczenia/Infografika.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/Infografika.java
@@ -4,20 +4,20 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Infografika {
-    String tytul;
-    String adres_strony;
-    String adres_grafiki;
-    String adres_miniaturki;
-    String szerokosc;
-    String wysokosc;
+    public String tytul;
+    public String adres_strony;
+    public String adres_grafiki;
+    public String adres_miniaturki;
+    public String szerokosc;
+    public String wysokosc;
 
     public Infografika(String tekst){
-        Pattern pat = Pattern.compile("<title><!>\\[CDATA\\[(.*)\\]\\]");
+        Pattern pat = Pattern.compile("<title><!\\[CDATA\\[(.*)\\]\\]");
         Matcher m = pat.matcher(tekst);
             if(m.find())
                 tytul=m.group(1);
             else
-                tytul="";
+                tytul="asd";
 
         pat = Pattern.compile("<link>(.*)</link>");
         m = pat.matcher(tekst);
@@ -28,28 +28,28 @@ public class Infografika {
 
 
 
-            pat = Pattern.compile("width=\"(.*)\"height");
+            pat = Pattern.compile("width=\"(.*)\"\\s*height");
             m = pat.matcher(tekst);
             if(m.find())
                 szerokosc=m.group(1);
             else
                 szerokosc= "0";
 
-           pat = Pattern.compile("width=\"(.*)\"height");
+           pat = Pattern.compile("height=\"(.*)\"");
            m = pat.matcher(tekst);
            if(m.find())
                 wysokosc=m.group(1);
            else
                 wysokosc= "0";
 
-            pat = Pattern.compile("width=\"(.*)\"height");
+            pat = Pattern.compile("<media:content\\s*url=\"(.*)\"\\s*type");
             m = pat.matcher(tekst);
             if(m.find())
                 adres_grafiki=m.group(1);
             else
                 adres_grafiki= "0";
 
-            pat = Pattern.compile("width=\"(.*)\"height");
+            pat = Pattern.compile("<media:thumbnail\\s*url=\"(.*)\"");
             m = pat.matcher(tekst);
             if(m.find())
                 adres_miniaturki=m.group(1);
--- src/main/java/pl/lublin/wsei/java/cwiczenia/TestInfografika.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/TestInfografika.java
@@ -4,8 +4,25 @@ import pl.lublin.wsei.java.cwiczenia.Infografika;
 
 public class TestInfografika {
     public static void main(String[] args) {
+        String exItem ="\t\t<item>\n" +
+                "\t\t\t<title><![CDATA[Infografika - Koniunktura gospodarcza w marcu 2022 r.]]></title>\n" +
+                "\t\t\t<pubDate><![CDATA[Tue, 22 Mar 2022 16:00:00 +0100]]></pubDate>\n" +
+                "\t\t\t<link>https://stat.gov.pl/infografiki-widzety/infografiki/infografika-koniunktura-gospodarcza-w-marcu-2022-r-,38,69.html</link>\n" +
+                "\t\t\t<guid isPermaLink=\"false\">https://stat.gov.pl/infografiki-widzety/infografiki/infografika-koniunktura-gospodarcza-w-marcu-2022-r-,38,69.html</guid>\n" +
+                "\t\t\t<media:content url=\"https://stat.gov.pl/files/gfx/portalinformacyjny/pl/defaultaktualnosci/5866/38/69/1/infografika_koniunktura_gospodarcza_03_2022.png\" type=\"image/png\" width=\"2362\" height=\"4548\">\n" +
+                "\t\t\t\t<media:description type=\"plain\"><![CDATA[]]></media:description>\n" +
+                "\t\t\t\t<media:thumbnail url=\"https://stat.gov.pl//gfx/portalinformacyjny/_thumbs/pl/defaultaktualnosci/5866/38/69/1/infografika_koniunktura_gospodarcza_03_2022,k1uUwl-caFOE6tCTiHtf.png\" />\n" +
+                "\t\t\t</media:content>\n" +
+                "\t\t\t<description><![CDATA[<div><img src=\"https://stat.gov.pl//gfx/portalinformacyjny/_thumbs/pl/defaultaktualnosci/5866/38/69/1/infografika_koniunktura_gospodarcza_03_2022,k1uUwl-caFOE6tCTiHtf.png\" alt=\"\" width=\"280\" height=\"212\"/></div>]]></description>\n" +
+                "\t\t</item>";
 
+        Infografika info1= new Infografika(exItem);
 
+        System.out.printf("Tytul:%s \n",info1.tytul);
+        System.out.printf("Adres strony:%s \n",info1.adres_strony);
+        System.out.printf("Adres grafiki:%s\n",info1.adres_grafiki);
+        System.out.printf("Adres miniaturki:%s\n",info1.adres_miniaturki);
+        System.out.printf("Rozmiar :%s x %s \n",info1.szerokosc,info1.wysokosc);
 
 
     }
